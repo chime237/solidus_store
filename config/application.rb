2d5e06ac6e4985f7e0218e01123d157492c2cb54
chime237 <nstrzeletz@gmail.com>
2015-12-29 02:24:47 +0000

    commit before changing database to pg

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TomegaSolidusStore
   class Application < Rails::Application
-    
+
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
